f655406e456133d94b008f3cd60e7de673c21f70
Justine <justine.denoeud@gmail.com>
2018-09-09 12:44:00 +0200

    Improve instantiation of the children classes

--- src/com/ocr/justine/Main.java
+++ src/com/ocr/justine/Main.java
@@ -4,33 +4,20 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
         // Create Player
-        for (int i = 1; i <= 2; i++) {
-            int role = createPlayer(i);
-            switch (role) {
-                case 1:
-                    Warrior warrior = new Warrior(i);
-                    warrior.describe();
-                    break;
-                case 2:
-                    Prowler prowler = new Prowler(i);
-                    prowler.describe();
-                    break;
-                case 3:
-                    Wizard wizard = new Wizard(i);
-                    wizard.describe();
-                    break;
-            }
+        Player player1;
+        Player player2;
+        player1 = createPlayer(1);
+        player1.describe();
+        player2 = createPlayer(2);
+        player2.describe();
         }
 
-
-        //Play
-    }
-
-    public static int createPlayer(int playerNumber) {
+    public static Player createPlayer(int playerNumber) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Création du personnage du Joueur "+ playerNumber);
+        System.out.println("Création du personnage du Joueur " + playerNumber);
         System.out.println("Veuillez choisir la classe de votre personnage (1 : Guerrier, 2 : Rôdeur, 3 : Mage)");
         int role = 0;
         boolean responseIsGood = false;
@@ -41,6 +28,20 @@ public class Main {
             else
                 System.out.println("Vous n'avez pas choisi parmi les 3 classes proposées");
         } while (!responseIsGood);
-        return role;
+        Player player = null;
+        switch (role) {
+            case 1:
+                player = new Warrior(playerNumber);
+
+                break;
+            case 2:
+                player = new Prowler(playerNumber);
+              break;
+            case 3:
+                player = new Wizard(playerNumber);
+            break;
+        }
+        return player;
+
     }
 }
