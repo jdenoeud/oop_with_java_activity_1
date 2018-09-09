40ec5e5e911ae59fed22bb22c16c4a2cfb64b8bb
Justine <justine.denoeud@gmail.com>
2018-09-09 14:56:55 +0200

    Optimize createPlayer

--- src/com/ocr/justine/Main.java
+++ src/com/ocr/justine/Main.java
@@ -9,9 +9,9 @@ public class Main {
 
     public static void main(String[] args) {
         // Create Player
-        Player player1;
+        Player player1 = createPlayer(1);
         Player player2;
-        player1 = createPlayer(1);
+
         player1.describe();
         player2 = createPlayer(2);
         player2.describe();
@@ -48,29 +48,20 @@ public class Main {
         System.out.println("Création du personnage du Joueur " + playerNumber);
         System.out.println("Veuillez choisir la classe de votre personnage (1 : Guerrier, 2 : Rôdeur, 3 : Mage)");
         int role = 0;
-        boolean responseIsGood = false;
         do {
             role = sc.nextInt();
-            if (role >= 1 && role <= 3)
-                responseIsGood = true;
-            else
-                System.out.println("Vous n'avez pas choisi parmi les 3 classes proposées");
-        } while (!responseIsGood);
-        Player player = null;
-        switch (role) {
-            case 1:
-                player = new Warrior(playerNumber);
-
-                break;
-            case 2:
-                player = new Prowler(playerNumber);
-              break;
-            case 3:
-                player = new Wizard(playerNumber);
-            break;
-        }
-        return player;
-    }
+            switch (role) {
+                case 1:
+                    return new Warrior(playerNumber);
+                case 2:
+                    return new Prowler(playerNumber);
+                case 3:
+                    return new Wizard(playerNumber);
+                default:
+                    System.out.println("Vous n'avez pas choisi parmi les 3 classes proposées");
+            }
+        } while (true);
+     }
 
 
 
